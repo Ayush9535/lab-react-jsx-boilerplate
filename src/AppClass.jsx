1c11f62985f54c3db4b3f6532a5ae28154eb3c08
Ayush9535 <ghodkeayush22@gmail.com>
2024-01-06 15:20:59 +0530

    first react app made

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -27,4 +27,31 @@ export default class AppClass extends Component{
   }
 
   // code here
+  render(){
+
+    let galleryStyle = {
+      textAlign:"center",
+      color:"red",
+      // fontFamily:"sans-serif",
+      // fontStyle:"italic" 
+    }
+  
+    let styleMyDiv = {
+      display:"grid",
+      gridTemplateColumns:"49.5% 49.5%",
+      gridGap:"1%"
+    }
+    this.data = this.imageData()
+    return (
+      <>
+        <h1 style={galleryStyle}>Kalvium Gallery Using Class Component</h1>
+        <div style={styleMyDiv}>
+          <img src={this.data[0].img} width={"100%"}/>
+          <img src={this.data[1].img} width={"100%"}/>
+          <img src={this.data[2].img} width={"100%"}/>
+          <img src={this.data[3].img} width={"100%"}/>
+        </div>
+      </>
+    )
+  }
 }
